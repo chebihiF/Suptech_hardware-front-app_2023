4f6919dc8f3d5cfa60cb71b960141cf39dfaf41a
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-23 20:25:48 +0100

    get data from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServerService } from './service/server.service';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { DataState } from './enum/data.state.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hardware-front-app';
+export class AppComponent implements OnInit {
+
+
+  appState$?: Observable<AppState<CustomResponse>>
+
+  constructor(private serverService: ServerService){}
+
+  ngOnInit(): void {
+
+    console.log("hello from AppComponent")
+
+    this.appState$ = this.serverService.servers$.
+    pipe(
+      map(response => {
+        console.log(response)
+        return { dataState: DataState.LOADED, appData: response }
+      }),
+      startWith({dataState: DataState.LOADING}),
+      catchError((error: string) => {
+        return of({dataState: DataState.ERROR, error})
+      })
+    )
+  }
+
 }
